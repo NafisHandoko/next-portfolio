6844d16727c21ecb2af9dc3d4675eb29db75d661
Nafis Handoko <naffisioner@gmail.com>
2024-07-20 18:56:40 +0700

    feat: added social media in mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -326,7 +326,14 @@ export default function Home() {
                 </p>
                 <a href="https://wa.me/6281332079137" target="_blank" className="text-nero border border-nero hover:bg-nero hover:text-codgray transition-all rounded-xl px-8 py-4 self-center">Let&apos;s Talk!</a>
             </section>
-            <footer className="container mx-auto flex flex-col items-center text-center py-7">
+            <footer className="container mx-auto flex flex-col gap-5 items-center text-center py-7">
+                <div className="flex flex-row md:hidden gap-5 text-nero">
+                    <a className="glow transition-all" target="_blank" href="https://github.com/nafishandoko"><FaGithub size={"25px"} /></a>
+                    <a className="glow transition-all" target="_blank" href="https://www.linkedin.com/in/nafishandoko"><FaLinkedin size={"25px"} /></a>
+                    <a className="glow transition-all" target="_blank" href="https://instagram.com/nafishandoko"><FaInstagram size={"25px"} /></a>
+                    <a className="glow transition-all" target="_blank" href="mailto:naffisioner@gmail.com"><FaRegEnvelope size={"25px"} /></a>
+                    <a className="glow transition-all" target="_blank" href="https://dribbble.com/nafishandoko"><FaDribbble size={"25px"} /></a>
+                </div>
                 <p className="text-nero">Made with ❤️ by Nafis Handoko</p>
             </footer>
         </>
