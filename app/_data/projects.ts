c05445c50e6242dec6f248a6217c18452eb0fbdf
Nafis Handoko <naffisioner@gmail.com>
2024-07-21 22:33:27 +0700

    style: added projects data

--- app/_data/projects.ts
+++ app/_data/projects.ts
@@ -16,11 +16,67 @@ export const projectsData = [
         image: '/assets/images/portfolio/eduwork.png'
     },
     {
+        name: 'Gits.id Old Website Clone',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        techStack: ['Web', 'HTML', 'CSS', 'Javascript'],
+        demoURL: 'https://nafishandoko.github.io/gits-test/',
+        repoURL: 'https://github.com/NafisHandoko/gits-test',
+        image: '/assets/images/portfolio/gits.png'
+    },
+    {
+        name: 'HaDe',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        techStack: ['Web', 'ReactJS', 'Tailwind CSS', 'Flask'],
+        demoURL: 'https://hade-matpro.netlify.app/',
+        repoURL: '',
+        image: '/assets/images/portfolio/hade.png'
+    },
+    {
+        name: 'Dahar',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        techStack: ['Mobile', 'Flutter', 'Firebase'],
+        demoURL: 'https://drive.google.com/drive/folders/1AGUN85hflvn9BBqQEan1iTEymz2K_yry?usp=sharing',
+        repoURL: 'https://github.com/NafisHandoko/dahar-flutter',
+        image: '/assets/images/portfolio/dahar-flutter.png'
+    },
+    {
+        name: 'DeboksKas',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        techStack: ['Web', 'Laravel', 'MySQL'],
+        demoURL: 'https://deboks-kas.000webhostapp.com/',
+        repoURL: 'https://github.com/NafisHandoko/deboks-kas',
+        image: '/assets/images/portfolio/debokskas.png'
+    },
+    {
+        name: 'NeuralBot',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        techStack: ['AI', 'ReactJS', 'Typescript', 'Tailwind CSS', 'Node JS'],
+        demoURL: 'https://neuralbot-fe.netlify.app/',
+        repoURL: 'https://github.com/NafisHandoko/neuralbot-fe',
+        image: '/assets/images/portfolio/neuralbot.png'
+    },
+    {
+        name: 'Depression Calculator',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        techStack: ['AI', 'Flask', 'LVQ'],
+        demoURL: 'https://depr-calc.nafishandoko.repl.co/',
+        repoURL: 'https://github.com/NafisHandoko/depr-calc',
+        image: '/assets/images/portfolio/depr-calc.png'
+    },
+    {
         name: 'Gotravel',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         techStack: ['Web', 'ReactJS', 'Tailwind CSS'],
         demoURL: 'https://react-gotravel.netlify.app/',
         repoURL: 'https://github.com/nafishandoko/gotravel',
         image: '/assets/images/portfolio/gotravel.png'
-    }
+    },
+    {
+        name: 'Cokro Collection Landing Page',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        techStack: ['Web', 'Bootstrap'],
+        demoURL: 'https://nafishandoko.github.io/cokrocollection/',
+        repoURL: 'https://github.com/NafisHandoko/cokrocollection',
+        image: '/assets/images/portfolio/cokrocollection.png'
+    },
 ]
