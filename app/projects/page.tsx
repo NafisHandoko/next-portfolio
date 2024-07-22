c64cc845a26c7ad540f932281caedcfa0532dd42
Nafis Handoko <naffisioner@gmail.com>
2024-07-22 15:00:00 +0700

    fix: fixed overlapping style in category button in projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,12 +12,12 @@ export default function ProjectsPage() {
         <div className="container mx-auto flex flex-col items-center px-10 md:px-40 py-24 pt-36 gap-16">
             <h2 className="uppercase text-nero font-bold text-3xl">Projects</h2>
             <div className="flex flex-row flex-wrap gap-4 items-center justify-center">
-                <Button type="tab" text="All" className={category == 'All' ? 'bg-nero text-codgray' : ''} onClick={() => setCategory('All')} />
-                <Button type="tab" text="No-Code" className={category == 'No-Code' ? 'bg-nero text-codgray' : ''} onClick={() => setCategory('No-Code')} />
-                <Button type="tab" text="Web" className={category == 'Web' ? 'bg-nero text-codgray' : ''} onClick={() => setCategory('Web')} />
-                <Button type="tab" text="Mobile" className={category == 'Mobile' ? 'bg-nero text-codgray' : ''} onClick={() => setCategory('Mobile')} />
-                <Button type="tab" text="UI/UX" className={category == 'UI/UX' ? 'bg-nero text-codgray' : ''} onClick={() => setCategory('UI/UX')} />
-                <Button type="tab" text="AI" className={category == 'AI' ? 'bg-nero text-codgray' : ''} onClick={() => setCategory('AI')} />
+                <Button type="tab" text="All" className={category == 'All' ? '!bg-nero !text-codgray' : ''} onClick={() => setCategory('All')} />
+                <Button type="tab" text="No-Code" className={category == 'No-Code' ? '!bg-nero !text-codgray' : ''} onClick={() => setCategory('No-Code')} />
+                <Button type="tab" text="Web" className={category == 'Web' ? '!bg-nero !text-codgray' : ''} onClick={() => setCategory('Web')} />
+                <Button type="tab" text="Mobile" className={category == 'Mobile' ? '!bg-nero !text-codgray' : ''} onClick={() => setCategory('Mobile')} />
+                <Button type="tab" text="UI/UX" className={category == 'UI/UX' ? '!bg-nero !text-codgray' : ''} onClick={() => setCategory('UI/UX')} />
+                <Button type="tab" text="AI" className={category == 'AI' ? '!bg-nero !text-codgray' : ''} onClick={() => setCategory('AI')} />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
                 {projectsData && category == 'All' ?
