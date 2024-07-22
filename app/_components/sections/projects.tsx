50d86861588884c319caaae017c5438c53fb07a2
Nafis Handoko <naffisioner@gmail.com>
2024-07-22 09:28:55 +0700

    feat: added slicing method to projects section, display only first 3 projects

--- app/_components/sections/projects.tsx
+++ app/_components/sections/projects.tsx
@@ -1,7 +1,5 @@
 import { projectsData } from "@/app/_data/projects";
 import Link from "next/link";
-import { FaGithub } from "react-icons/fa6";
-import { GoLinkExternal } from "react-icons/go";
 import ProjectsCard from "../widgets/projects/card";
 
 export default function ProjectsSection() {
@@ -9,7 +7,7 @@ export default function ProjectsSection() {
         <section id="projects" className="container mx-auto flex flex-col px-10 md:px-40 py-24 gap-10">
             <h2 className="uppercase text-nero font-bold text-3xl">Projects</h2>
             <div className="flex flex-col">
-                {projectsData && projectsData.map((project: any, index: any) => <ProjectsCard project={project} key={index} index={index} />)}
+                {projectsData && projectsData.slice(0, 3).map((project: any, index: any) => <ProjectsCard project={project} key={index} index={index} />)}
             </div>
             <Link href="/projects" className="text-nero border border-nero hover:bg-nero hover:text-codgray transition-all rounded-md px-8 py-4 self-center">Show more</Link>
         </section>
