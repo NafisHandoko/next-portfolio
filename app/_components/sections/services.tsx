68514a4cb18f8f59c4ddf97d950269e224bf1f7d
Nafis Handoko <naffisioner@gmail.com>
2024-07-22 17:30:57 +0700

    style: make a better ux/copy in services section

--- app/_components/sections/services.tsx
+++ app/_components/sections/services.tsx
@@ -13,7 +13,7 @@ export default function ServicesSection() {
                     <div className="flex flex-col p-7 gap-3">
                         <FaLaptopCode className="text-nero text-3xl" />
                         <h3 className="font-bold text-xl text-nero">Web Development</h3>
-                        <p className="text-nero">I can help you build a good looking website with stunning animation</p>
+                        <p className="text-nero">I can transform your idea into a website with captivating animations and sleek design, creating an unforgettable user experience.</p>
                     </div>
                     <div className="flex flex-row items-center p-7 gap-3 text-nero bg-codgray rounded-b-md mt-auto">
                         <SiNextdotjs size={"25px"} />
@@ -26,7 +26,7 @@ export default function ServicesSection() {
                     <div className="flex flex-col p-7 gap-3">
                         <BsPhone className="text-nero text-3xl" />
                         <h3 className="font-bold text-xl text-nero">Mobile Development</h3>
-                        <p className="text-nero">I am able to create mobile app with Flutter that can help you solve your problem</p>
+                        <p className="text-nero">Let me build a powerful mobile app with Flutter to seamlessly address your specific challenges and streamline your processes, providing an intuitive and effective solution.</p>
                     </div>
                     <div className="flex flex-row items-center p-7 gap-3 text-nero bg-codgray rounded-b-md mt-auto">
                         <RiFlutterFill size={"25px"} />
@@ -36,7 +36,7 @@ export default function ServicesSection() {
                     <div className="flex flex-col p-7 gap-3">
                         <MdOutlinePalette className="text-nero text-3xl" />
                         <h3 className="font-bold text-xl text-nero">UI/UX Design</h3>
-                        <p className="text-nero">I can help you build a complete prototye of your products</p>
+                        <p className="text-nero">I can help you build a fully functional UI prototype of your product, turning your ideas into a tangible, testable reality.</p>
                     </div>
                     <div className="flex flex-row items-center p-7 gap-3 text-nero bg-codgray rounded-b-md mt-auto">
                         <FaFigma size={"25px"} />
