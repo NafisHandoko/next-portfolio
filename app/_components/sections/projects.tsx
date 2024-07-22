aced96cb9d68dc10b3435606ef30cfb102fcad2a
Nafis Handoko <naffisioner@gmail.com>
2024-07-22 14:16:35 +0700

    feat!: removed original link in projects section

--- app/_components/sections/projects.tsx
+++ app/_components/sections/projects.tsx
@@ -1,5 +1,4 @@
 import { projectsData } from "@/app/_data/projects";
-import Link from "next/link";
 import ProjectsCard from "../widgets/projects/card";
 import Button from "../base/button";
 
@@ -10,7 +9,6 @@ export default function ProjectsSection() {
             <div className="flex flex-col">
                 {projectsData && projectsData.slice(0, 3).map((project: any, index: any) => <ProjectsCard project={project} key={index} index={index} />)}
             </div>
-            {/* <Link href="/projects" className="text-nero border border-nero hover:bg-nero hover:text-codgray transition-all rounded-md px-8 py-4 self-center">Show more</Link> */}
             <Button type="Link" href="/projects" text="Show more" className="self-center" />
         </section>
     )
