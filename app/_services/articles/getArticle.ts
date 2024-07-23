ede0456bb50e77c292b56e572acdeae89df8f2a7
Nafis Handoko <naffisioner@gmail.com>
2024-07-23 21:30:57 +0700

    feat: added cache to getArticle service

--- app/_services/articles/getArticle.ts
+++ app/_services/articles/getArticle.ts
@@ -1,12 +1,13 @@
+import { cache } from "react"
 import githubBlogWrapper from "./githubBlogWrapper"
 
-const getArticle = async (slug: string) => {
+const getArticle = cache(async (slug: string) => {
     const article = await githubBlogWrapper.getPost({
         query: {
             search: slug,
         }
     })
     return article.post
-}
+})
 
 export default getArticle
