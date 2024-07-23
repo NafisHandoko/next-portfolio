48822f73fd1e100b7c4f0e94fefd166958e1a89f
Nafis Handoko <naffisioner@gmail.com>
2024-07-23 12:08:53 +0700

    feat: added other articles section in article detail page

--- app/(pages)/articles/[slug]/page.tsx
+++ app/(pages)/articles/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import ArticlesCard2 from "@/app/_components/widgets/articles/card2";
+import { articlesData } from "@/app/_data/articles";
+
 export default function ArticleDetailPage() {
     return (
         <div>
@@ -7,23 +10,31 @@ export default function ArticleDetailPage() {
                     <span className="text-silver">May 12, 2024</span>
                 </div>
             </div>
-            <div className="flex flex-row gap-7 py-20 px-10 md:px-40">
-                <div className="w-full md:w-3/4 text-nero">
-                    Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.
+            <div className="flex flex-col px-10 md:px-40 divide-y divide-mineshaft">
+                <div className="flex flex-row gap-7 py-20">
+                    <div className="w-full md:w-3/4 text-nero">
+                        Lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.
+                    </div>
+                    <div className="w-1/4 hidden md:flex flex-col gap-4">
+                        <span className="text-nero font-bold text-xl">Table of Contents</span>
+                        <div className="flex flex-col gap-3 text-sm">
+                            <span className="font-semibold text-nero">Nextjs base folder structure</span>
+                            <span className="text-jumbo">What is Atomic Design</span>
+                            <span className="text-jumbo">Atomic Design structure</span>
+                            <span className="text-jumbo ml-7">Atoms</span>
+                            <span className="text-jumbo ml-7">Molecules</span>
+                            <span className="text-jumbo ml-7">Organisms</span>
+                            <span className="text-jumbo ml-7">Templates</span>
+                            <span className="text-jumbo">Atomic Design best practices</span>
+                            <span className="text-jumbo">Examples</span>
+                            <span className="text-jumbo">Closure</span>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-1/4 hidden md:flex flex-col gap-4">
-                    <span className="text-nero font-bold text-xl">Table of Contents</span>
-                    <div className="flex flex-col gap-3 text-sm">
-                        <span className="font-semibold text-nero">Nextjs base folder structure</span>
-                        <span className="text-jumbo">What is Atomic Design</span>
-                        <span className="text-jumbo">Atomic Design structure</span>
-                        <span className="text-jumbo ml-7">Atoms</span>
-                        <span className="text-jumbo ml-7">Molecules</span>
-                        <span className="text-jumbo ml-7">Organisms</span>
-                        <span className="text-jumbo ml-7">Templates</span>
-                        <span className="text-jumbo">Atomic Design best practices</span>
-                        <span className="text-jumbo">Examples</span>
-                        <span className="text-jumbo">Closure</span>
+                <div className="flex flex-col py-20 gap-10">
+                    <h2 className="text-nero font-bold text-3xl">Other articles</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                        {articlesData && articlesData.map((article: any, i: any) => <ArticlesCard2 article={article} key={i} />)}
                     </div>
                 </div>
             </div>
