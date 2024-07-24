d32f765bcc0aa0bc1fdbac68f6acdc9c550cbaed
Nafis Handoko <naffisioner@gmail.com>
2024-07-24 15:01:37 +0700

    fix: fixed sticky toc issue

--- app/(pages)/articles/[slug]/page.tsx
+++ app/(pages)/articles/[slug]/page.tsx
@@ -16,8 +16,8 @@ export default async function ArticleDetailPage({ params }: { params: { slug: st
                 </div>
             </div>
             <div className="flex flex-col px-10 md:px-40 divide-y divide-mineshaft">
-                <div className="flex flex-row gap-7 py-20">
-                    <div className="w-1/4 hidden md:flex flex-col gap-4">
+                <div className="flex flex-row gap-7 py-20 items-start">
+                    <div className="w-1/4 hidden md:flex flex-col gap-4 my-0 sticky top-28">
                         <span className="text-nero font-bold text-xl">Table of Contents</span>
                         <div className="flex flex-col gap-3 text-sm">
                             <span className="font-semibold text-nero">Nextjs base folder structure</span>
