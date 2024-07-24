afd2be62673b58f5cdb8008c29c73f6b8b71f9c1
Nafis Handoko <naffisioner@gmail.com>
2024-07-24 23:29:33 +0700

    feat: added framer-motion

---
+++ app/_components/base/framer.ts
@@ -0,0 +1,12 @@
+"use client"
+
+import { motion } from "framer-motion"
+
+export const {
+    div: MotionDiv,
+    h1: MotionH1,
+    h2: MotionH2,
+    p: MotionP,
+    section: MotionSection,
+    header: MotionHeader
+} = motion
