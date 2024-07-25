b7e661fda7dee7701ce5650bead53ece020e6f2e
Nafis Handoko <naffisioner@gmail.com>
2024-07-25 21:55:57 +0700

    feat: added text scramble animation in hero section using use-scramble library

--- app/_components/sections/hero.tsx
+++ app/_components/sections/hero.tsx
@@ -3,8 +3,20 @@ import { ReactTyped } from "react-typed";
 import Button from "../base/button";
 import { MotionDiv, MotionH1, MotionHeader } from "../base/framer";
 import { fadeInTransition, staggeredContainer, textVariantTransition } from "@/app/_utils/motion";
+import { useScramble } from "use-scramble";
 
 export default function HeroSection() {
+    const { ref, replay } = useScramble({
+        text: 'Nafis Handoko',
+        speed: 0.7,
+        tick: 1,
+        step: 1,
+        scramble: 10,
+        seed: 0,
+        overdrive: false,
+        range: [65, 90]
+    });
+
     return (
         <MotionHeader
             variants={staggeredContainer(0.1, 0.1)}
@@ -14,17 +26,21 @@ export default function HeroSection() {
             id="header"
             className="bg-mineshaft text-nero"
         >
-            <div className="min-h-screen container mx-auto w-full flex flex-col items-center justify-center gap-y-7 text-center px-10 md:px-0">
-                <MotionH1 variants={textVariantTransition(0.2)} className="font-bold text-4xl">
-                    Hi i am&nbsp;
-                    <ReactTyped
-                        strings={['Nafis Handoko', 'a Software Engineer', 'a Web Developer', 'a Mobile Developer', 'a UI/UX Designer']}
-                        typeSpeed={60}
-                        backSpeed={40}
-                        // attr="placeholder"
-                        loop
-                    />
-                </MotionH1>
+            <div className="min-h-screen container mx-auto w-full flex flex-col items-center justify-center gap-y-10 text-center px-10 md:px-0">
+                <div className="flex flex-col items-center gap-2">
+                    <span className="text-silver">Hi! i am</span>
+                    <h1 className="font-bold text-4xl uppercase tracking-widest" ref={ref} onMouseOver={replay}>Nafis Handoko</h1>
+                    <p className="text-silver">
+                        A self-taught&nbsp;
+                        <ReactTyped
+                            strings={['Software Engineer', 'Web Developer', 'Mobile Developer', 'UI/UX Designer']}
+                            typeSpeed={60}
+                            backSpeed={40}
+                            // attr="placeholder"
+                            loop
+                        />
+                    </p>
+                </div>
                 <MotionDiv
                     variants={fadeInTransition("up", "tween", 0.3, 0.7)}
                 >
