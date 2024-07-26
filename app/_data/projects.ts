03fe437ceee95ab568998a2fd727bbc965ab2167
Nafis Handoko <naffisioner@gmail.com>
2024-07-26 23:43:06 +0700

    style: updated projects content

--- app/_data/projects.ts
+++ app/_data/projects.ts
@@ -1,7 +1,7 @@
 export const projectsData = [
     {
         name: 'Teknik Kimia Universitas Jember',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: 'This is a website that contains information about the chemical engineering study program at University of Jember such as contacts, locations, lecturers and alumni, along with the latest student news and activities. I created this website using WordPress and the popular WordPress plugin, Elementor.',
         techStack: ['No-Code', 'Wordpress', 'Elementor'],
         demoURL: 'https://chemeng.teknik.unej.ac.id/',
         repoURL: '',
@@ -9,7 +9,7 @@ export const projectsData = [
     },
     {
         name: 'Eduwork Sosmed',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: "Eduwork Sosmed is a website I worked on during my internship at Eduwork.id. This website is used to build a community and promote Eduwork's latest products. On this website users can explore webinars, courses, and mentors available at Eduwork.",
         techStack: ['Web', 'ReactJS', 'Typescript', 'Redux', 'Tailwind CSS', 'React Native', 'Alpine.js', 'Laravel'],
         demoURL: 'https://sosmed.edudev.xyz/',
         repoURL: '',
@@ -17,7 +17,7 @@ export const projectsData = [
     },
     {
         name: 'Speedside Landing Page',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: 'A landing page that i built for Speedside. Speedside is a small software house company based in Indonesia that offers services such as website development, mobile application development, UI/UX design, and IT consulting.',
         techStack: ['Web', 'Next.js', 'Typescript', 'Tailwind CSS'],
         demoURL: 'https://speedside.vercel.app/',
         repoURL: 'https://github.com/nafishandoko/speedside',
@@ -25,7 +25,7 @@ export const projectsData = [
     },
     {
         name: 'Gits.id Old Website Clone',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: 'This is an old version of Gits.id website. Cloning Gits.id website is a part of technical test to apply for Software Engineer Intern position in Gits.id. I am using pure and vanilla HTML, CSS, and Javascript to build this website. No framework/library involved except for the typing animation i am using typed.js.',
         techStack: ['Web', 'HTML', 'CSS', 'Javascript'],
         demoURL: 'https://nafishandoko.github.io/gits-test/',
         repoURL: 'https://github.com/NafisHandoko/gits-test',
@@ -33,7 +33,7 @@ export const projectsData = [
     },
     {
         name: 'HaDe',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: 'Hade is a website I created during my internship at Bisa.ai. HaDe is an e-commerce website that sells various necessities for building a house such as building materials and construction workers.',
         techStack: ['Web', 'ReactJS', 'Tailwind CSS', 'Flask'],
         demoURL: 'https://hade-matpro.netlify.app/',
         repoURL: '',
@@ -41,7 +41,7 @@ export const projectsData = [
     },
     {
         name: 'Dahar',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: 'Dahar is a food delivery mobile application specifically designed for owners of traditional Javanese culinary businesses. In this project i learned a lot of things such as how to work with maps using GPS, Google Maps, and OpenRouteService to measure distances. I am also using Firebase and Firestore to store data and Firebase Auth to handle authentication.',
         techStack: ['Mobile', 'Flutter', 'Firebase'],
         demoURL: 'https://drive.google.com/drive/folders/1AGUN85hflvn9BBqQEan1iTEymz2K_yry?usp=sharing',
         repoURL: 'https://github.com/NafisHandoko/dahar-flutter',
@@ -49,15 +49,15 @@ export const projectsData = [
     },
     {
         name: 'DeboksKas',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        techStack: ['Web', 'Laravel', 'MySQL'],
+        description: 'DeboksKas is a website I created for my client, a banana stem chips seller in Jombang Regency. With this application users can record finances, employees, stalls, and products sold. In addition to recording, users can also see financial performance on a weekly or monthly basis and see which stalls sell the best-selling products.',
+        techStack: ['Web', 'Laravel', 'MySQL', 'jQuery'],
         demoURL: 'https://deboks-kas.000webhostapp.com/',
         repoURL: 'https://github.com/NafisHandoko/deboks-kas',
         image: '/assets/images/portfolio/debokskas.png'
     },
     {
         name: 'NeuralBot',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: 'A ChatGPT clone. This is just a random fun project i made while exploring OpenAI API (the free plan of course). Built using React-Typescript and Tailwindcss for the frontend and Node JS for the backend.',
         techStack: ['AI', 'ReactJS', 'Typescript', 'Tailwind CSS', 'Node JS'],
         demoURL: 'https://neuralbot-fe.netlify.app/',
         repoURL: 'https://github.com/NafisHandoko/neuralbot-fe',
@@ -65,15 +65,15 @@ export const projectsData = [
     },
     {
         name: 'Depression Calculator',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        techStack: ['AI', 'Flask', 'LVQ'],
+        description: 'Depression Calculator is a platform to detect potential levels of depression. Here you can diagnose how much potential depression you are currently experiencing. This application works using the Learning Vector Quantization method which makes predictions based on data such as age, gender, and also characteristics or symptoms that you can enter in the form.',
+        techStack: ['AI', 'Python', 'Flask', 'LVQ'],
         demoURL: 'https://depr-calc.nafishandoko.repl.co/',
         repoURL: 'https://github.com/NafisHandoko/depr-calc',
         image: '/assets/images/portfolio/depr-calc.png'
     },
     {
         name: 'Gotravel Landing Page',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: 'This is the website I created while holding a live webinar on Bisa.ai. I did live coding by transforming the website design from Figma into a live website deployed to Netlify. You can watch the video here https://www.youtube.com/watch?v=GXb9lagiQTc',
         techStack: ['Web', 'ReactJS', 'Tailwind CSS'],
         demoURL: 'https://react-gotravel.netlify.app/',
         repoURL: 'https://github.com/nafishandoko/gotravel',
@@ -81,7 +81,7 @@ export const projectsData = [
     },
     {
         name: 'Cokro Collection Landing Page',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: "This is a simple landing page that I built at my friend's request for his father's textile business.",
         techStack: ['Web', 'Bootstrap'],
         demoURL: 'https://nafishandoko.github.io/cokrocollection/',
         repoURL: 'https://github.com/NafisHandoko/cokrocollection',
