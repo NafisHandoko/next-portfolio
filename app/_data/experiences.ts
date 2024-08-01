13bde457a916dfacc52d3bb193b1964bfee98eee
Nafis Handoko <naffisioner@gmail.com>
2024-08-01 13:49:45 +0700

    style: updated experiences data

--- app/_data/experiences.ts
+++ app/_data/experiences.ts
@@ -5,7 +5,7 @@ export const experiencesData = [
         companyName: 'University of Jember',
         companyURL: 'https://unej.ac.id',
         type: 'Freelance',
-        description: 'Responsible for helping a client build a website using WordPress to display information and the latest news about the Chemical Engineering program at the University of Jember.',
+        description: 'Responsible for helping a client to build a website using WordPress to display information and the latest news about the Chemical Engineering program at the University of Jember.',
         techStack: ['Wordpress', 'Elementor']
     },
     {
@@ -14,7 +14,7 @@ export const experiencesData = [
         companyName: 'Eduwork.id',
         companyURL: 'https://eduwork.id/',
         type: 'Internship',
-        description: 'Spearheaded the development of the Eduwork student dashboard with the CTO using React, Typescript, Redux, and Tailwind CSS. Played a key role in delivering the Eduwork Sosmed website with a team, using React JS, React Native, Tailwind CSS, Alpine.js, and Laravel. Authored project documentation for knowledge transfer, participated in daily scrum meetings with the Project Manager and CEO, and mentored new interns.',
+        description: 'Collaborated with CTO to develop second iteration of Eduwork student dashboard using React, Typescript, Redux, and Tailwind CSS. Played a key role in delivering Eduwork Sosmed website with a team, using React JS, React Native, Tailwind CSS, Alpine.js, and Laravel. Authored project documentation for knowledge transfer, participated in daily scrum meetings with the Project Manager and CEO, and mentored new interns.',
         techStack: ['ReactJS', 'Typescript', 'Redux', 'Tailwind CSS', 'React Native', 'Alpine.js', 'Laravel']
     },
     {
@@ -23,7 +23,7 @@ export const experiencesData = [
         companyName: 'Gits.id',
         companyURL: 'https://gits.id/',
         type: 'Internship',
-        description: 'Wrote unit tests using Jest and Vue Test Utils for MyPertamina project and assisted with the frontend of the POC Prodia-LIS project using the Syncfusion React Spreadsheet library. On the Carteapp project, ensured the frontend adhered to the UI design in Figma and participated in validation. Became a guest speaker for the webinar "Unit Test with Jest & Vue Test Utils" sharing insights with 15-30 participants. Recognized as "Best Mentees" upon graduation for my dedication to professional growth.',
+        description: 'Wrote unit tests using Jest and Vue Test Utils for MyPertamina project and assisted with the frontend of POC Prodia-LIS project using the Syncfusion React Spreadsheet library. On the Carteapp project, ensured the frontend adhered to the UI design in Figma and participated in validation. Became a guest speaker for the webinar "Unit Test with Jest & Vue Test Utils" sharing insights with 15-30 participants. Recognized as "Best Mentees" upon graduation for my dedication to professional growth.',
         techStack: ['ReactJS', 'Nuxt 3', 'Tailwind CSS', 'Typescript', 'Jest']
     },
     {
@@ -32,7 +32,7 @@ export const experiencesData = [
         companyName: 'Bisa.ai',
         companyURL: 'http://bisa.ai/',
         type: 'Internship',
-        description: 'Contributed to the Hade Matpro Project, collaborating with Backend Programmers and UI/UX Designers to develop components using ReactJS and Tailwind CSS. Spoke at webinars "Strategi SEO pada ReactJS" and "From Design to Code with React & Tailwind" for 30-40 participants. Aided API development using Flask and helped design user-friendly interfaces. Pursued courses in UI/UX Design and AI to enhance my skills.',
+        description: 'Contributed to the Hade Matpro Project, collaborating with Backend Programmers and UI/UX Designers to develop components using ReactJS and Tailwind CSS. Spoke at webinars "SEO Strategy in ReactJS" and "From Design to Code with React & Tailwind" for 30-40 participants. Aided API development using Flask and helped design user-friendly interfaces. Pursued courses in UI/UX Design and AI to enhance my skills.',
         techStack: ['ReactJS', 'Tailwind CSS', 'Flask']
     },
 ]
