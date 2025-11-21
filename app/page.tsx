06afa455f5b552a17ca63833bb11228be0624776
Nafis Handoko <naffisioner@gmail.com>
2025-11-21 21:54:39 +0700

    setup docker for self-hosted deployment

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             <ExperiencesSection />
             <ServicesSection />
             <ProjectsSection />
-            <ArticlesSection />
+            {/* <ArticlesSection /> */}
             <ContactSection />
         </>
     );
