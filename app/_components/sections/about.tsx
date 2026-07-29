da33cc336bc4e8654ffc780b56d32aad36f20cb4
Nafis Handoko <naffisioner@gmail.com>
2026-07-29 23:06:13 +0700

    feat: implement BuildKit cache for faster build process

--- app/_components/sections/about.tsx
+++ app/_components/sections/about.tsx
@@ -17,7 +17,7 @@ export default function AboutSection() {
                 variants={fadeInTransition("right", "tween", 0.1, 1.1)}
                 className="w-full md:w-1/2 flex flex-col gap-10"
             >
-                <MotionH2 variants={textVariantTransition(0.3)} className="uppercase text-light-nero dark:text-dark-nero font-bold text-3xl">About Me - Tes</MotionH2>
+                <MotionH2 variants={textVariantTransition(0.3)} className="uppercase text-light-nero dark:text-dark-nero font-bold text-3xl">About Me</MotionH2>
                 <MotionP variants={textVariantTransition(0.4)} className="text-light-silver dark:text-dark-silver">
                     Hello there!
                     <br />
